19dff93e3d4d45bfd336060da3bb5dbbedd39d47
Sergio Martínez <SerchOnFire@users.noreply.github.com>
2019-08-09 01:45:45 -0500

    Kenney textures + Game scene created + Behaviour scripts

---
+++ Assets/Scripts/001-BrickBreaker/BrickBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBehaviour : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void OnMouseDown()
+    {
+        KillBrick();
+    }
+
+    void KillBrick()
+    {
+        Destroy(this.gameObject);
+    }
+    
+
+}
